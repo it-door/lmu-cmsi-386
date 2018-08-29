9f28d666e5044b692d55cb32f39ef1c3120e4523
it-door <tedchu4@gmail.com>
2018-08-28 18:16:53 -0700

    Added function names

---
+++ homework1/src/warmup.js
@@ -0,0 +1,39 @@
+function change(){
+
+}
+
+function stripQuotes(){
+
+}
+
+function scramble(){
+
+}
+
+function powers(){
+
+}
+
+function powersGenerator(){
+
+}
+
+function say(){
+
+}
+
+function interleave(){
+
+}
+
+function cylinder(){
+
+}
+
+function makeCryptoFunctions(){
+
+}
+
+function randomName(){
+    
+}
