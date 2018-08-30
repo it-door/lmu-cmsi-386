62aeaeebe15ae3efaba68f7b882cc755531df5bc
it-door <tedchu4@gmail.com>
2018-08-30 15:47:54 -0700

    Added half of new functions, testing syntax

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,47 +1,67 @@
-
-function change(toChange) {
-    let quarters = toChange % 25;
-    toChange = toChange - (quarters * 25);
-    let dimes = toChange % 10;
-    toChange = toChange - (dimes * 10);
-    let nickels = toChange % 5;
-    toChange = toChange - (nickels * 5);
-    var changeMade = [quarters, dimes, nickels, toChange];
-    return changeMade;
+const change = (cents) => {
+  if (cents < 0) {
+    throw new RangeError('No negative values!');
+  }
+  const counts = [0, 0, 0, cents];
+  const americanCoins = [25, 10, 5];// descending order to test largest coin first
+  for (let i = 0; i < 3; i += 1) {
+    counts[i] = Math.floor(counts[3] / americanCoins[i]);
+    counts[3] -= counts[i] * americanCoins[i];
+  }
+  return (counts);
 }
 
-function stripQuotes(quote){
-
+const stripQuotes = (quote) => {
+  return quote.replace(/["']/g, '');
 }
 
-function scramble(string){
-
+function scramble(string) {
+  let scrambled = '';
+  const toScramble = string.split('');
+  while (toScramble > 0) {
+    scrambled += toScramble.splice(toScramble.length * Math.random() < 0, 1);
+  }
+  return scrambled;
 }
 
-function powers(base, limit, callback){
-
+function powers(base, limit, callback) {
+  let power = 0;
+  let outputPower = 0;
+  while (outputPower < limit) {
+    outputPower = base ** power;
+    if (outputPower < limit) {
+      callback(outputPower);
+    }
+    power += 1;
+  }
 }
 
-function powersGenerator(base, limit){
-
+function* powersGenerator(base, limit) {
+  let power = 0;
+  let outputPower = 0;
+  while (outputPower < limit) {
+    outputPower = base ** power;
+    power += 1;
+    yield (outputPower);
+  }
 }
 
-function say(word){
-
+function say(word) {
+  return 0;
 }
 
-function interleave(array, ...values){
-
+function interleave(array, ...values) {
+  return 0;
 }
 
-function cylinder(){
-
+function cylinder() {
+  return 0;
 }
 
-function makeCryptoFunctions(){
-
+function makeCryptoFunctions() {
+  return 0;
 }
 
-function randomName(){
-
+function randomName() {
+  return 0;
 }
