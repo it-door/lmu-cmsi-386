e445dc261da439a8a56bee4fca8e63d059bbd87f
it-door <tedchu4@gmail.com>
2018-08-30 20:01:03 -0700

    Cleaned up first 5 functions and started cylinders

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,17 +1,17 @@
 function change(cents) {
-  try {
-    const counts = [0, 0, 0, cents];
-    const americanCoins = [25, 10, 5]; // descending order to test largest coin first
-
-    for (let i = 0; i < 3; i += 1) {
-      counts[i] = Math.floor(counts[3] / americanCoins[i]);
-      counts[3] -= counts[i] * americanCoins[i];
-    }
-
-    return (counts);
-  } catch (err) {
+  if (cents < 0) {
     throw new RangeError();
   }
+
+  const counts = [0, 0, 0, cents];
+  const americanCoins = [25, 10, 5]; // descending order to test largest coin first
+
+  for (let i = 0; i < 3; i += 1) {
+    counts[i] = Math.floor(counts[3] / americanCoins[i]);
+    counts[3] -= counts[i] * americanCoins[i];
+  }
+
+  return (counts);
 }
 
 function stripQuotes(quote) {
@@ -23,7 +23,7 @@ function scramble(string) {
   const toScramble = string.split('');
 
   while (toScramble.length > 0) {
-    scrambled += toScramble.splice(toScramble.length * Math.random() < 0, 1);
+    scrambled += toScramble.splice(toScramble.length * Math.random() << 0, 1);
   }
 
   return scrambled;
@@ -31,26 +31,25 @@ function scramble(string) {
 
 function powers(base, limit, callback) {
   let power = 0;
-  let outputPower = 0;
+  let outputPower = base ** power;
+  power += 1;
 
-  while (outputPower < limit) {
+  while (outputPower <= limit) {
+    callback(outputPower);
     outputPower = base ** power;
-    if (outputPower < limit) {
-      callback(outputPower);
-    }
-
     power += 1;
   }
 }
 
 function* powersGenerator(base, limit) {
   let power = 0;
-  let outputPower = 0;
+  let outputPower = base ** power;
+  power += 1;
 
-  while (outputPower < limit) {
+  while (outputPower <= limit) {
+    yield (outputPower);
     outputPower = base ** power;
     power += 1;
-    yield (outputPower);
   }
 }
 
@@ -62,8 +61,16 @@ function interleave(array, ...values) {
   return 0;
 }
 
-function cylinder() {
-  return 0;
+function cylinder(spec) {
+  let { radius, height } = spec;
+  const surfaceArea = () => 2 * Math.PI * radius * height + 2 * Math.PI * radius * radius;
+  const volume = () => Math.PI * radius * radius * height;
+  const widen = (factor) => { radius *= factor; };
+  const stretch = (factor) => { height *= factor; };
+  const toString = () => `Cylinder with radius ${radius} and height ${height}`;
+  return Object.freeze({
+    surfaceArea, volume, widen, stretch, toString,
+  });
 }
 
 function makeCryptoFunctions() {
@@ -73,3 +80,16 @@ function makeCryptoFunctions() {
 function randomName() {
   return 0;
 }
+
+module.exports = {
+  change,
+  stripQuotes,
+  scramble,
+  cylinder,
+  randomName,
+  say,
+  makeCryptoFunctions,
+  interleave,
+  powers,
+  powersGenerator,
+};
