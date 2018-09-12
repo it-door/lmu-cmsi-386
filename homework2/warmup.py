83f367ef5294e885c1bb389a4e354520e8c2912e
it-door <tedchu4@gmail.com>
2018-09-11 17:12:18 -0700

    Finished triples() and powers(), added interleave()

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -1,5 +1,6 @@
 import math
 import random
+import Crypto
 
 def change(cents):
     if cents < 0:
@@ -18,16 +19,37 @@ def scramble(string):
     return ''.join(random.sample(string, len(string)))
 
 def say():
-    pass
+    if word == '':
+        return ''
+    def curry(otherWord):
+      if (otherWord == undefined):
+        return word
+      return say(word % " " % otherWord)
 
-def triples():
-    pass
+def triples(n):
+    return [(c,b,a) for a in range(n+1) for b in range(a) for c in range(b) if a*a == b*b + c*c]
 
-def powers():
-    pass
+def powers(base, limit):
+    power = 0
+    outputPower = base ** power
+    power += 1
 
-def interleave():
-    pass
+    while outputPower <= limit:
+      yield (outputPower)
+      outputPower = base ** power
+      power += 1
+
+def interleave(array, *values):
+    interleaved = []
+    for i,j in zip(range(len(array)), range(len(values))):
+      if i < len(array):
+        interleaved.append(array[i])
+        i += 1
+
+      if j < len(values):
+        interleaved.append(values[j])
+        j += 1
+    return interleaved
 
 def Cylinder():
     pass
