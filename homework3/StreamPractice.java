37405c65e01181656f2877a517b5ed70a4ba7d1c
it-door <tedchu4@gmail.com>
2018-11-01 17:32:28 -0700

    Added more template code and input files

--- homework3/StreamPractice.java
+++ homework3/StreamPractice.java
@@ -1,17 +1,37 @@
-import java.util.TreeMap;
-import java.util.regex.Pattern;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+// IMPORTS GO HERE
 
 public class StreamPractice {
-    public static void main(String[] args) {
-        var nonWord = Pattern.compile("[^\\p{L}']+");
-        new BufferedReader(new InputStreamReader(System.in))
-            .lines()
-            .flatMap(line -> nonWord.splitAsStream(line.toLowerCase()))
-            .filter(word -> !word.isEmpty())
-            .collect(Collectors.groupingBy(w->w, TreeMap::new, Collectors.counting()))
-            .forEach((word, count) -> System.out.printf("%s %d\n", word, count));
+
+    static Pattern nonWord = Pattern.compile("[^\\p{L}']+");
+
+    public static Map<Integer, Long> wordCountByLength(BufferedReader reader) {
+        // Return a tree map whose keys are word lengths
+        // and corresponding values are the number of words
+        // in the reader with that length.
+    }
+
+    public static class Batter {
+        String name;
+        String team;
+        int atBats;
+        int hits;
+        double average;
+        Batter(String line) {
+            String[] components = line.split("\\s*,\\s*");
+            this.name = components[0];
+            this.team = components[1];
+            this.atBats = Integer.parseInt(components[2]);
+            this.hits = Integer.parseInt(components[3]);
+            this.average = (double)this.hits / (double)this.atBats;
+        }
+        @Override public String toString() {
+          return this.name + " " + this.average;
+        }
+    }
+
+    public static Map<String, Optional<Batter>> bestBatterByTeam(BufferedReader reader) {
+        // Return a map that records, for each team, the batter with
+        // the highest average over all batters that have at least
+        // 100 at-bats.
     }
 }
